a285c16a5a3ff119f4041a5f19574517340f3898
hungvq98 <hung.vq98@gmail.com>
2022-07-15 13:49:14 +0700

    bang keo va tin tuc chi tiet

--- template/js/module/moduleSwiper.js
+++ template/js/module/moduleSwiper.js
@@ -1,120 +1,26 @@
 export default function moduleSwiper() {
-    var swiper = new Swiper(".mySwiper", {
-        spaceBetween: 10,
-        slidesPerView: 4,
-        freeMode: true,
-        watchSlidesProgress: true
-    });
-    var swiper2 = new Swiper(".mySwiper2", {
-        spaceBetween: 10,
-        autoplay: true,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev"
-        },
-        thumbs: {
-            swiper: swiper
-        }
-    });
-
-    var swiperHome = new Swiper(".sliderSwiper", {
-        slidesPerView: 2.5,
+    var swiper = new Swiper(".brandSwiper", {
+        slidesPerView: 6,
         spaceBetween: 30,
-        loop: true,
-        autoplay: true,
-
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true
-        },
-        breakpoints: {
-            0: {
-                slidesPerView: 1
-            },
-            768: {
-                slidesPerView: 2
-            },
-            1050: {
-                slidesPerView: 3
-            },
-            1200: {
-                slidesPerView: 2.5
-            }
-        }
-    });
-
-    var swiperfeel = new Swiper(".feelSwiper", {
-        slidesPerView: 3.5,
-        spaceBetween: 30,
-        loop: true,
-        autoplay: true,
-        breakpoints: {
-            0: {
-                slidesPerView: 1
-            },
-            500: {
-                slidesPerView: 1.5
-            },
-            768: {
-                slidesPerView: 2.5
-            },
-            1200: {
-                slidesPerView: 3.5
-            }
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true
-        }
-    });
-
-    var swiperSlider = new Swiper(".newsSlider-swiper", {
-        slidesPerView: 4,
-        spaceBetween: 24,
         centeredSlides: true,
-        loop: true,
-        autoplay: true,
+        loop:true,
         pagination: {
-            el: ".swiper-pagination",
-            clickable: true
+          el: ".swiper-pagination",
+          clickable: true,
         },
         breakpoints: {
-            0: {
-                slidesPerView: 1
+            320: {
+              slidesPerView: 1.5,
             },
-            500: {
-                slidesPerView: 1.5
-            },
-            768: {
-                slidesPerView: 2.5
-            },
-            1200: {
-                slidesPerView: 4
-            }
-        }
-    });
-    var swiperSliders = new Swiper(".newsSmall-swiper", {
-        slidesPerView: 3,
-        spaceBetween: 24,
-        loop: true,
-        autoplay: true,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true
-        },
-        breakpoints: {
-            0: {
-                slidesPerView: 1
-            },
-            500: {
-                slidesPerView: 1.5
+            426: {
+              slidesPerView: 2,
             },
             768: {
-                slidesPerView: 2.5
+              slidesPerView: 4,
             },
             1200: {
-                slidesPerView: 3
-            }
-        }
-    });
+              slidesPerView: 6,
+            },
+          },
+      });
 }
