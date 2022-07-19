e1dbdbd8af83ebf9bfb75aedba346eb785d6c3f5
hungvq98 <hung.vq98@gmail.com>
2022-07-19 14:03:42 +0700

    banner home

--- template/js/module/moduleSwiper.js
+++ template/js/module/moduleSwiper.js
@@ -1,26 +1,32 @@
 export default function moduleSwiper() {
-    var swiper = new Swiper(".brandSwiper", {
+  var swiper = new Swiper(".brandSwiper", {
+    slidesPerView: 6,
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1.5,
+      },
+      426: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      1200: {
         slidesPerView: 6,
-        spaceBetween: 30,
-        centeredSlides: true,
-        loop:true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        breakpoints: {
-            320: {
-              slidesPerView: 1.5,
-            },
-            426: {
-              slidesPerView: 2,
-            },
-            768: {
-              slidesPerView: 4,
-            },
-            1200: {
-              slidesPerView: 6,
-            },
-          },
-      });
+      },
+    },
+  });
+  var swiper = new Swiper(".homeSwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 }
