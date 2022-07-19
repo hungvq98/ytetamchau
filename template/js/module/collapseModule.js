7f1b983592b86c1e746e7a455b5c75536dec02a7
chotr <minhphungbmt141999@gmail.com>
2022-07-19 09:20:43 +0700

    fix click collapse

--- template/js/module/collapseModule.js
+++ template/js/module/collapseModule.js
@@ -28,15 +28,34 @@ export default function () {
     });
   }
   if (lstShape) {
-    const lstShapeInfo = document.querySelectorAll(".shape-info-content");
-    lstShape.forEach((ele, i) => {
-      let zindex = 30;
-      ele.style.zIndex = zindex - i
-      $(ele).hover(function () {
-        $(lstShapeInfo[i]).stop().slideToggle(300, () => {
-
-        });
+    const createCollapse = () => {
+      const windowWidth = window.innerWidth;
+      const lstShapeInfo = document.querySelectorAll(".shape-info-content");
+      lstShape.forEach((ele, i) => {
+        let zindex = 30;
+        ele.style.zIndex = zindex - i;
+        if (windowWidth > 1024) {
+          console.log("> 1024")
+          $(ele).hover(function () {
+            $(lstShapeInfo[i])
+              .stop()
+              .slideToggle(300, () => {});
+          });
+        } else {
+          console.log("< 1024")
+          $(ele).click(function () {
+            $(lstShapeInfo[i])
+              .stop()
+              .slideToggle(300, () => {});
+          });
+        }
       });
+    };
+
+    createCollapse();
+
+    window.addEventListener("resize", () => {
+      createCollapse();
     });
   }
 }
