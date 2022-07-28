7d994407c46a8da1e69afb948324d1258f966539
chotr <minhphungbmt141999@gmail.com>
2022-07-28 10:50:06 +0700

    new css

--- wp-content/themes/monatheme/js/front.js
+++ wp-content/themes/monatheme/js/front.js
@@ -1,44 +1,107 @@
 import Header from "./modules/header.js";
 Header();
 
-jQuery(".color-lst")
-  .find(".color-control")
-  .each(function (index, element) {
+jQuery(".color-lst").find(".color-control").each(function (index, element) {
     var color = $(this).find(".custom-color").data("color");
     $(this).find(".custom-color").css("background", color);
-  });
+});
 
-jQuery(".info")
-  .find(".color-item")
-  .each(function (index, element) {
+jQuery(".info").find(".color-item").each(function (index, element) {
     var color = $(this).data("color-child");
     $(this).css("background", color);
-  });
+});
 
 $(".bandage-pro-wrap .list .list-item").each(function (index, element) {
-  $(this).click(function (e) {
-    e.preventDefault();
-    var id = $(this).data("id");
-    $('[data-number="' + id + '"]').addClass("active");
-    $(".bg-fade").addClass("show");
-    document.body.style.overflow = "hidden";
-  });
+    $(this).find(".circle-search").click(function (e) { 
+        e.preventDefault();
+        var id = $(this).closest(".list-item").data("id");
+        $('[data-number="'+id+'"]').addClass("active");
+        $(".bg-fade").addClass("show");
+        document.body.style.overflow = "hidden";
+    });
+    
+    $(this).find(".circle-link").click(function (e) { 
+        e.preventDefault();
+        var link = $(this).data("link-product");
+        var $temp = $("<input>");
+        $("body").append($temp);
+        $temp.val(link).select();
+        document.execCommand("copy");
+        $temp.remove();
+        $(this).attr('title', "Đã copy");
+    });
+});
+
+$(".related-list .swiper-slide").each(function (index, element) {
+    $(this).find(".circle-search").click(function (e) { 
+        e.preventDefault();
+        var id = $(this).closest(".swiper-slide").data("id");
+        $('[data-number="'+id+'"]').addClass("active");
+        $(".bg-fade").addClass("show");
+        document.body.style.overflow = "hidden";
+    });
+
+    $(this).find(".circle-link").click(function (e) { 
+        e.preventDefault();
+        var link = $(this).data("link-product");
+        var $temp = $("<input>");
+        $("body").append($temp);
+        $temp.val(link).select();
+        document.execCommand("copy");
+        $temp.remove();
+        $(this).attr('title', "Đã copy");
+    });
 });
 
 $(".equipment-detail .equipment-product").each(function (index, element) {
-  $(this).click(function (e) {
-    e.preventDefault();
-    var id = $(this).data("id");
-    $('[data-equipment="' + id + '"]').addClass("active");
-    $(".bg-fade").addClass("show");
-    document.body.style.overflow = "hidden";
-  });
+    $(this).click(function (e) { 
+        e.preventDefault();
+        var id = $(this).data("id");
+        $('[data-equipment="'+id+'"]').addClass("active");
+        $(".bg-fade").addClass("show");
+        document.body.style.overflow = "hidden";
+    });
 });
 
-$(".close").click(function (e) {
-  e.preventDefault();
-  $(this).closest(".popup-detail").removeClass("active");
-  $(this).closest(".popup-equipment").removeClass("active");
-  $(".bg-fade").removeClass("show");
-  document.body.style.overflow = "auto";
+$(".close").click(function (e) { 
+    e.preventDefault();
+    $(this).closest(".popup-detail").removeClass("active");
+    $(this).closest(".popup-equipment").removeClass("active");
+    $(".bg-fade").removeClass("show");
+    document.body.style.overflow = "auto";
 });
+
+document.addEventListener('mouseup', function (e) {
+    var container = document.querySelector('.popup-detail');
+    if(container) {
+        if (!container.contains(e.target)) {
+            $(".popup-detail").removeClass("active");
+            $(".bg-fade").removeClass("show");
+            document.body.style.overflow = "auto";
+        }
+    }
+}.bind(this));
+
+document.addEventListener('mouseup', function (e) {
+    var container = document.querySelector('.popup-equipment');
+    if (container) {
+        if (!container.contains(e.target)) {
+            $(".popup-equipment").removeClass("active");
+            $(".bg-fade").removeClass("show");
+            document.body.style.overflow = "auto";
+        }
+        
+    }
+}.bind(this));
+
+document.addEventListener('mouseup', function (e) {
+    var container = document.querySelector('.popup-cv');
+    if (container) {
+        if (!container.contains(e.target)) {
+            $(".popup-cv").removeClass("active");
+            $(".bg-fade").removeClass("show");
+            document.body.style.overflow = "auto";
+        }
+        
+    }
+}.bind(this));
