f1c2dc3ecf4aef482df9c489a1dd75178fa4cb1b
hungvq98 <hung.vq98@gmail.com>
2022-07-29 17:35:36 +0700

    fb update

--- template/js/module/listItemModule.js
+++ template/js/module/listItemModule.js
@@ -5,14 +5,16 @@ export default function listItemModule() {
             var imgPro = item.querySelectorAll('.img-pro');
             imgPro[0].classList.add('active');
             var btnColor = item.querySelectorAll('.btn-color');
-            for (let i = 0; i < btnColor.length; i++) {
-                const element = btnColor[i];
-                element.addEventListener('click', () => {
-                    imgPro.forEach(item => {
-                        item.classList.remove('active')
+            if(imgPro.length == btnColor.length) {
+                for (let i = 0; i < btnColor.length; i++) {
+                    const element = btnColor[i];
+                    element.addEventListener('click', () => {
+                        imgPro.forEach(item => {
+                            item.classList.remove('active')
+                        })
+                        imgPro[i].classList.add('active')
                     })
-                    imgPro[i].classList.add('active')
-                })
+                }
             }
         })
 
